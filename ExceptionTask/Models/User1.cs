2085d659b63a9780478634481dedf81920c7ddad
seferovrusif1 <mi73sbd9a@code.edu.az>
2023-11-03 02:10:42 +0400

    sd

---
+++ ExceptionTask/Models/User1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTask.Models
+{
+    internal class User1
+    {
+        private int _id { get; set; } =0;
+        private string _name { get; set; } = "";
+        private static int _age { get; set; } = 0;
+        public string a;
+        public static void ChangeAge(int age) 
+        {
+            _age = age;
+        }
+
+        
+    }
+}
